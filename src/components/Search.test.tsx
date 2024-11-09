00e75aa110a141cb5bfefcae24b215ebb67887af
Spielberg <javiersanchezostiz@gmail.com>
2024-11-09 12:07:53 +0100

    fix coverage test in search input

--- src/components/Search.test.tsx
+++ src/components/Search.test.tsx
@@ -1,10 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import * as React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
 import Search from './Search';
 
 import es from '@/helper/i18n/translation/es.json';
 
 describe('Search Component', () => {
+  const SearchWithState = () => {
+    const [filter, setFilter] = React.useState('');
+    return <Search filter={filter} setFilter={setFilter} />;
+  };
+
   it('should render the search input with placeholder', () => {
     render(<Search />);
 
@@ -17,5 +23,19 @@ describe('Search Component', () => {
 
     const iconButton = screen.getByRole('button');
     expect(iconButton).toBeInTheDocument();
+  });
+
+  it('should update the input value', () => {
+    render(<SearchWithState />);
+
+    const inputElement = screen.getByPlaceholderText(es['app.search.input.placeholder']) as HTMLInputElement;
+
+    fireEvent.change(inputElement, { target: { value: 'test' } });
+
+    expect(inputElement.value).toBe('test');
+
+    fireEvent.change(inputElement, { target: { value: '' } });
+
+    expect(inputElement.value).toBe('');
   });
 });
